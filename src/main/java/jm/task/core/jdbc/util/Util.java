a305fba2f2d9d54f76dea3b22d9fc2ae4452cac1
Schapkov <106531210+Schapkov@users.noreply.github.com>
2022-09-16 19:24:36 +0300

    changed getSessionFactory

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -2,7 +2,6 @@ package jm.task.core.jdbc.util;
 
 import jm.task.core.jdbc.model.User;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
 import java.sql.Connection;
@@ -11,7 +10,7 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class Util {
-    private static Util instance;
+    private static SessionFactory sessionFactory;
 
     private final static String PATH = "jdbc:mysql://localhost:3306/mydb?serverTimezone=Europe/Moscow";
     private final static String LOGIN = "root";
@@ -22,34 +21,26 @@ public class Util {
         return DriverManager.getConnection(PATH, LOGIN, PASSWORD);
     }
 
-    public static SessionFactory sessionFactory () {
-        Util instance = Util.getInstance();
-        return instance.makeSessionFactory();
-    }
+    public static SessionFactory getSessionFactory () {
+        if (sessionFactory == null) {
+            Properties properties = new Properties();
+            properties.setProperty("hibernate.connection.url", PATH);
+            properties.setProperty("dialect", "org.hibernate.dialect.MySQLDialect");
+            properties.setProperty("hibernate.connection.username", LOGIN);
+            properties.setProperty("hibernate.connection.password", PASSWORD);
+            properties.setProperty("hibernate.connection.driver_class", DRIVER);
+            properties.setProperty("show_sql", "true");
 
-    private static Util getInstance() {
-        if(instance == null) {
-            instance = new Util();
+            sessionFactory = new Configuration()
+                    .addProperties(properties)
+                    .addAnnotatedClass(User.class)
+                    .buildSessionFactory();
+            return sessionFactory;
         }
-        return instance;
-    }
-
-    private static SessionFactory makeSessionFactory () {
-        Properties properties = new Properties();
-        properties.setProperty("hibernate.connection.url", PATH);
-        properties.setProperty("dialect", "org.hibernate.dialect.MySQLDialect");
-        properties.setProperty("hibernate.connection.username", LOGIN);
-        properties.setProperty("hibernate.connection.password", PASSWORD);
-        properties.setProperty("hibernate.connection.driver_class", DRIVER);
-        properties.setProperty("show_sql", "true");
-
-        SessionFactory sessionFactory = new Configuration()
-                .addProperties(properties)
-                .addAnnotatedClass(User.class)
-                .buildSessionFactory();
 
         return sessionFactory;
     }
+
 
 
 }
